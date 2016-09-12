a3ae2f96468f4a7d16e04d8adf747971522f2563
Amar Bhamra <Amar.Bhamra@zion.local>
2016-09-12 14:41:29 +0100

    refactor

--- index.js
+++ index.js
@@ -2,26 +2,23 @@
 var button = document.getElementById('button');
 var p = document.getElementById('paragraph');
 
+var setButtonColour = setColour(button);
+var changeColour = setColourOnClick(button, p);
+
+
 function setColour(elem) {
     return function (color) {
         return elem.style.color = color
     };
 };
 
-var setButtonColour = setColour(button);
-
-function getById(name) {
-    return document.getElementById(name);
-}
-
 function setColourOnClick(elem, newP){
     return function(colour){
         return elem.addEventListener('click', function () {
             setColour(newP)(colour);
         });
-    }
-}
+    };
+};
 
-var setParagraphColour = setColourOnClick(button, p);
-
-setParagraphColour('#00FF00');
+setButtonColour('#0000FF');
+changeColour('#00FF00');
